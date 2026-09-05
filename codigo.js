4a109dafa80b802e1f9a5d12462a1962b9a7f578
João Paulo <dev.joaopaulocosta@gmail.com>
2026-09-05 05:11:16 -0300

    acabei?

---
+++ codigo.js
@@ -0,0 +1,33 @@
+let slideAtual = 0;
+
+const slides = document.querySelectorAll(".pictures");
+const container = document.querySelector(".slides");
+
+const esquerda = document.querySelector(".setaesquerda");
+const direita = document.querySelector(".setadireita");
+
+
+direita.addEventListener("click", function () {
+
+    slideAtual++;
+
+    if (slideAtual >= slides.length) {
+        slideAtual = 0;
+    }
+
+    container.style.transform =
+        `translateX(-${slideAtual * 100}%)`;
+});
+
+
+esquerda.addEventListener("click", function () {
+
+    slideAtual--;
+
+    if (slideAtual < 0) {
+        slideAtual = slides.length - 1;
+    }
+
+    container.style.transform =
+        `translateX(-${slideAtual * 100}%)`;
+});
